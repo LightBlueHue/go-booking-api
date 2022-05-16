8660392b681348b7d22c8bb4dcca4c277efc7c34
LightBlueHue <83064850+LightBlueHue@users.noreply.github.com>
2022-05-16 18:45:53 +0100

    more booking service tests

--- app/services/booking_service_test.go
+++ app/services/booking_service_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"go-booking-api/app/models"
 	"regexp"
 	"testing"
 
@@ -66,7 +67,7 @@ func Test_Book_WhenDbError_Returns_Error(t *testing.T) {
 	actualBookingId, actualError := target.Book(userId, count)
 
 	assert.Empty(t, actualBookingId)
-	assert.Equal(t, expectedError.Error(), actualError.Error())
+	assert.Equal(t, expectedError, actualError)
 }
 
 func Test_Book_WhenDbBookingIdZero_Returns_Error(t *testing.T) {
@@ -95,5 +96,60 @@ func Test_Book_WhenDbBookingIdZero_Returns_Error(t *testing.T) {
 	actualBookingId, actualError := target.Book(userId, count)
 
 	assert.Empty(t, actualBookingId)
-	assert.Equal(t, expectedError.Error(), actualError.Error())
+	assert.Equal(t, expectedError, actualError)
+}
+
+func Test_GetBookings_Returns_Data(t *testing.T) {
+
+	var db *gorm.DB
+	var setupError error
+
+	sqlDb, sqlMock, _ := sqlmock.New()
+	defer sqlDb.Close()
+	userId := uint(faker.RandomInt(0, 100))
+	expectedBookings := []models.Booking{{UserID: 1, TicketInventoryID: 1, Tickets: 16}}
+
+	sqlMock.ExpectQuery(regexp.QuoteMeta("user_id = $1")).
+		WithArgs(userId).
+		WillReturnRows(sqlmock.NewRows([]string{"UserID", "TicketInventoryID", "Tickets"}).
+			AddRow(expectedBookings[0].UserID, expectedBookings[0].TicketInventoryID, expectedBookings[0].Tickets))
+
+	db, setupError = gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDb,
+	}), &gorm.Config{})
+
+	assert.Nil(t, setupError)
+	target := GetBookingService(db)
+
+	actualBookings, actualError := target.GetBookings(userId)
+
+	assert.Equal(t, &expectedBookings, actualBookings)
+	assert.Nil(t, actualError)
+}
+
+func Test_GetBookings_Returns_Error(t *testing.T) {
+
+	var db *gorm.DB
+	var setupError error
+
+	sqlDb, sqlMock, _ := sqlmock.New()
+	defer sqlDb.Close()
+	userId := uint(faker.RandomInt(0, 100))
+	expectedError := errors.New("sb")
+
+	sqlMock.ExpectQuery(regexp.QuoteMeta("user_id = $1")).
+		WithArgs(userId).
+		WillReturnError(expectedError)
+
+	db, setupError = gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDb,
+	}), &gorm.Config{})
+
+	assert.Nil(t, setupError)
+	target := GetBookingService(db)
+
+	actualBookings, actualError := target.GetBookings(userId)
+
+	assert.Empty(t, actualBookings)
+	assert.Equal(t, expectedError, actualError)
 }
