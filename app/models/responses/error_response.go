6bf1eed43b8d42eae1eb6f4f86671fb2a2caf398
LightBlueHue <83064850+LightBlueHue@users.noreply.github.com>
2022-08-21 19:07:10 +0100

    error response documentation

--- app/models/responses/error_response.go
+++ app/models/responses/error_response.go
@@ -1,9 +1,11 @@
 package responses
 
+// ErrorResponse contains response information about an error that occured for a request.
 type ErrorResponse struct {
 	Error Error `json:"error"`
 }
 
+// Error contains information such as http status code and error details.
 type Error struct {
 	Code    string        `json:"code"`
 	Message string        `json:"message"`
@@ -11,6 +13,7 @@ type Error struct {
 	Details []ErrorDetail `json:"details"`
 }
 
+// ErrorDetail conatins detailed error information.
 type ErrorDetail struct {
 	Code    string `json:"code"`
 	Message string `json:"message"`
