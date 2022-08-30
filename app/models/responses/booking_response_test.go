c0dcf14544804dceebce34ed18d8199233330d6d
LightBlueHue <83064850+LightBlueHue@users.noreply.github.com>
2022-08-30 17:17:16 +0100

    booking response test

--- app/models/responses/booking_response_test.go
+++ app/models/responses/booking_response_test.go
@@ -1,1 +1,25 @@
 package responses_test
+
+import (
+	"go-booking-api/app/models/responses"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"syreclabs.com/go/faker"
+)
+
+func TestBookingResponse_Behaves_Correctly(t *testing.T) {
+
+	// Arrange
+	expectedBookingNumber := uint(faker.RandomInt(0, 100))
+	expectedName := faker.Name().Name()
+	expectedTickets := uint(faker.RandomInt(0, 100))
+
+	// Act
+	actual := responses.BookingResponse{BookingNumber: uint(expectedBookingNumber), Name: expectedName, Tickets: uint(expectedTickets)}
+
+	// Assert
+	assert.Equal(t, expectedBookingNumber, actual.BookingNumber)
+	assert.Equal(t, expectedName, actual.Name)
+	assert.Equal(t, expectedTickets, actual.Tickets)
+}
